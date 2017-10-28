7dfae37e7767262e507a1e5e89228cf6a80b539f
SilverBut <SilverBut@users.noreply.github.com>
2017-10-28 19:19:17 +0800

    Add debug related code and some dev shortcuts.

--- code/main.cpp
+++ code/main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <sstream>
+#include <vector>
+#include <unistd.h>
+#include <cassert>
+
 int main(int argc, char *argv[]) {
 
   std::cout << "Welcome to LCTF 2017 Old Driver Platform!" << std::endl;
@@ -7,7 +12,7 @@ int main(int argc, char *argv[]) {
   std::cout << "!!! UNDER DEBUG MODE !!!" << std::endl;
 #endif
 
-  if (argc != 6+1) {
+  if (argc != 6 + 1) {
     std::cout << "Seems incorrect parameters are used?" << std::endl;
 #ifndef DEBUG
     return EINVAL;
@@ -22,8 +27,17 @@ int main(int argc, char *argv[]) {
   // TODO Use methods in this link to put those file into the executable file
   // TODO  in order to add some complexity:
   // TODO  https://stackoverflow.com/questions/4864866/c-c-with-gcc-statically-add-resource-files-to-executable-library
+#ifdef DEBUG
+  std::cout << "!!! Debug Hint: Will not use external arguments. Will use hard-code files." << std::endl;
+
+  // Add address for example file
+  std::string MapSourceFile("../examples");
+  std::string OpSourceFile("../");
+
+#else
   std::string MapSourceFile(argv[5]);
   std::string OpSourceFile(argv[6]);
+#endif
 
   std::cout << "Input parameters: " << std::endl;
   std::cout << " - Token:         " << Token << std::endl;
@@ -32,6 +46,17 @@ int main(int argc, char *argv[]) {
   std::cout << " - OpOffsetFIle:  " << OpOffset << std::endl;
   std::cout << " - MapSourceFile: " << MapSourceFile << std::endl;
   std::cout << " - OpSourceFile:  " << OpSourceFile << std::endl;
+
+#ifdef DEBUG
+  std::cout << "Debug Environment Info:" << std::endl;
+  const unsigned int bufferSize = 512;
+  std::vector<char> _buffer(bufferSize + 1);
+  ssize_t count = readlink("/proc/self/exe", &_buffer[0], bufferSize);
+  assert(count != -1);
+  _buffer[count - 1] = 0;
+  std::string buffer = &_buffer[0];
+  std::cout << " - Current File: " << buffer << std::endl;
+#endif
 
 
   return 0;
